a28a5cde609f7a6276f4e20b329e9ced45bf1232
Chen Song <csong2022@berkeley.edu>
2019-04-25 12:33:11 -0400

    Add bfs-dfs and test cases.

---
+++ algorist/graph/graph.py
@@ -0,0 +1,101 @@
+from algorist.data_structure.linked_list import List
+
+
+class UnweightedEdgeNode:
+    def __init__(self, y):
+        self.y = y  # adjancency info
+
+    def copy(self, v):
+        return UnweightedEdgeNode(v)
+
+
+class WeightedEdgeNode:
+    def __init__(self, y, weight):
+        self.y = y  # adjancency info
+        self.weight = weight  # edge weight
+
+    def copy(self, v):
+        return WeightedEdgeNode(v, self.weight)
+
+
+class Graph:
+    def __init__(self, nvertices, directed):
+        self.edges = [List() for i in range(nvertices + 1)]  # adjacency info
+        self.degree = [0] * (nvertices + 1)  # outdegree of each vertex
+
+        self.nvertices = nvertices  # number of vertices in the graph
+        self.nedges = 0  # number of edges in the graph
+        self.directed = directed  # is the graph directed?
+
+    def find_edge(self, x, y):
+        self.validate_vertex(x)
+        self.validate_vertex(y)
+        return next((p for p in self.edges[x] if p.y == y), None)
+
+    def validate_vertex(self, v):
+        if v < 1 or v > self.nvertices:
+            raise IndexError('Invalid vertex %d' % v)
+
+    def insert_edge(self, x, n, directed):
+        self.validate_vertex(x)
+        self.validate_vertex(n.y)
+
+        self.edges[x].insert(n)
+        self.degree[x] += 1
+
+        if not directed:
+            self.insert_edge(n.y, n.copy(x), True)
+        else:
+            self.nedges += 1
+
+    def delete_edge(self, x, y, directed):
+        self.validate_vertex(x)
+        self.validate_vertex(y)
+
+        n = self.find_edge(x, y)
+
+        if n is not None:
+            self.edges[x].delete(n)
+            self.degree[x] -= 1
+            if not directed:
+                self.delete_edge(y, x, True)
+            else:
+                self.nedges -= 1
+        else:
+            print("Warning: deletion(%d,%d) not found in g" % (x, y))
+
+    def print(self):
+        for i in range(1, self.nvertices + 1):
+            print("%d: " % i),
+
+            for p in self.edges[i]:
+                print(" %d", p.y),
+            print()
+
+
+class UnweightedGraphReader:
+    def read_graph(self, input, directed):
+        nvertices, nedges = list(map(int, input.readline().split()))
+
+        g = Graph(nvertices, directed)
+
+        for i in range(nedges):
+            x, y = list(map(int, input.readline().split()))
+            n = UnweightedEdgeNode(y)
+            g.insert_edge(x, n, directed)
+
+        return g
+
+
+class WeightedGraphReader:
+    def read_graph(self, input, directed):
+        nvertices, nedges = list(map(int, input.readline().split()))
+
+        g = Graph(nvertices, directed)
+
+        for i in range(nedges):
+            x, y, w = list(map(int, input.readline().split()))
+            n = WeightedEdgeNode(y, w)
+            g.insert_edge(x, n, directed)
+
+        return g
---
+++ tests/algorist/graph/dfs_demo.py
@@ -0,0 +1,53 @@
+from unittest import TestCase
+
+from algorist.graph.bfs_dfs import EdgeType, DFS
+from algorist.graph.graph import UnweightedGraphReader
+from tests.algorist.test.test_engine import execute
+
+
+class DFSDemoCallback:
+    def __init__(self, dfs):
+        self.dfs = dfs
+
+    def process_vertex_early(self, v):
+        self.dfs.time += 1
+        self.dfs.entry_time[v] = self.dfs.time
+        print("entered vertex %d at time %d" % (v, self.dfs.entry_time[v]))
+
+    def process_vertex_late(self, v):
+        self.dfs.time += 1
+        self.dfs.exit_time[v] = self.dfs.time
+        print("exit vertex %d at time %d" % (v, self.dfs.exit_time[v]));
+
+    def process_edge(self, x, y):
+        classification = self.dfs.edge_classification(x, y);
+
+        if classification == EdgeType.BACK:
+            print("back edge (%d,%d)" % (x, y))
+        elif classification == EdgeType.TREE:
+            print("tree edge (%d,%d)" % (x, y))
+        elif classification == EdgeType.FORWARD:
+            print("forward edge (%d,%d)" % (x, y))
+        elif classification == EdgeType.CROSS:
+            print("cross edge (%d,%d)" % (x, y))
+        else:
+            print("edge (%d,%d)\n not in valid class=%d" % (x, y, classification))
+
+
+class DFSDemoTest(TestCase):
+    def process(self, input):
+        reader = UnweightedGraphReader()
+        g = reader.read_graph(input, False)
+        g.print()
+
+        dfs = DFS(g)
+        callback = DFSDemoCallback(dfs)
+        dfs.search(1, callback)
+
+        print()
+        for i in range(1, g.nvertices + 1):
+            path = dfs.find_path(1, i)
+            print(path, end=' ')
+
+    def test(self):
+        execute(self, "grid", "grid-dfs-demo-out")
