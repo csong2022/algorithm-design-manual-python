db10a5cababf771609d95ef723aa6cb55c48e872
Chen Song <csong2022@berkeley.edu>
2019-04-25 13:02:14 -0400

    Add connected and test cases.

---
+++ tests/algorist/graph/test_connected.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+
+from algorist.graph.connected import connected_components
+from algorist.graph.graph import GraphReader
+from tests.algorist.test.test_engine import execute
+
+
+class TestConnected(TestCase):
+    def process(self, input):
+        reader = GraphReader.unweighted()
+        g = reader.read_graph(input, False)
+        g.print()
+
+        connected_components(g)
+
+    def test_connected(self):
+        execute(self, "connected-in", "connected-out")
+
+    def test_grid(self):
+        execute(self, "grid", "grid-connected-out")
