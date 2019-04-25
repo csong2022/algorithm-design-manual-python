497339b2074d423714b35cb13b056b27cfa3c5cc
Chen Song <csong2022@berkeley.edu>
2019-04-25 17:01:11 -0400

    Add prim and test cases.

---
+++ algorist/wgraph/prim.py
@@ -0,0 +1,33 @@
+MAXINT = 100007;
+
+
+def prim(g, start):
+    """
+    ompute minimum spanning trees of graphs via Dijkstra/Prim's algorithm.
+    :param g: weighted graph
+    :param start: starting vertex
+    :return: discovery relation array
+    """
+
+    parent = [-1] * (g.nvertices + 1)  # discovery relation *
+    intree = [False] * (g.nvertices + 1)  # is the vertex in the tree yet?
+    distance = [MAXINT] * (g.nvertices + 1)  # cost of adding to tree
+
+    distance[start] = 0
+
+    v = start  # current vertex to process
+    while not intree[v]:
+        intree[v] = True
+
+        for p in g.edges[v]:
+            w, weight = p.y, p.weight
+            if distance[w] > weight and (not intree[w]):
+                distance[w] = weight
+                parent[w] = v
+
+        v, dist = 1, MAXINT
+        for i in range(1, g.nvertices + 1):
+            if (not intree[i]) and dist > distance[i]:
+                v, dist = i, distance[i]
+
+    return parent
