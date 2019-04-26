55bb62961f1fb0ee8b348d1be628a08ca57d07df
Chen Song <csong2022@berkeley.edu>
2019-04-25 22:44:33 -0400

    Add binomial.

---
+++ algorist/dp/binomial.py
@@ -0,0 +1,23 @@
+def binomial_coefficient(n, m):
+    """
+    Compute the binomial coefficients using dynamic programming.
+
+    :param n:
+    :param m:
+    :return:
+    """
+    assert m <= n
+
+    bc = [[0] * (n + 1) for i in range(n + 1)]
+
+    for i in range(n + 1):
+        bc[i][0] = 1
+
+    for j in range(n + 1):
+        bc[j][j] = 1
+
+    for i in range(n + 1):
+        for j in range(1, i):
+            bc[i][j] = bc[i - 1][j - 1] + bc[i - 1][j]
+
+    return bc[n][m]
